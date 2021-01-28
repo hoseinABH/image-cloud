46225c9e07fab6ef35280351ecbe6e6814c82f8d
unknown <hosein.abolhasani1998@gmail.com>
2021-01-28 13:47:15 +0330

    added <Navbar />

---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="bg-gray-900 dark:bg-green-50">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="flex items-center w-full justify-between h-20">
+        <div class="">
+          <img
+            class="h-10 w-16 transform hover:-rotate-90 duration-500 cursor-pointer"
+            src="../assets/mastery.png"
+            alt="vue gallery"
+          />
+        </div>
+        <div class="ml-10 flex items-baseline space-x-4">
+          <!-- <a
+            href="#"
+            class=" text-white duration-300  hover:text-green-500 text-sm font-medium"
+            >Gallery</a
+          >
+          <a
+            href="#"
+            class=" text-white duration-300 hover:text-green-500 text-sm font-medium"
+            >Upload</a
+          > -->
+          <button
+            class="px-4 py-1 font-bold text-gray-100 duration-300  bg-green-600 focus:outline-none rounded shadow hover:bg-green-500"
+          >
+            Signin
+          </button>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+};
+</script>
